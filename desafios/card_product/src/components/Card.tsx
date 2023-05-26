02d855eb6dd07f606a7dc792d9f0f9ee693b1e0a
Edvan Ribeiro <jk.jr47@gmail.com>
2023-05-26 00:54:49 -0300

    finalizado proposta do desafio

--- desafios/card_product/src/components/Card.tsx
+++ desafios/card_product/src/components/Card.tsx
@@ -1,16 +1,29 @@
 import style from "./Card.module.css"
-import sofa from "../assets/sofa.png"
-import viewProduct from "../assets/view-product.svg"
+import productImg from "../assets/sofa.png"
+import productGit from "../assets/sofa.gif"
+import view from "../assets/view-product.svg"
+import close from '../assets/close.svg'
+import { useState } from "react"
 
 
 export function Card() {
+    const [viewProduct, setWiewProduct] = useState(false);
+
+    function handleClick() {
+        setWiewProduct(!viewProduct)
+    }
+
     return (
         <div className={style.card}>
             <div className={style.imageContainer}>
                 <div className={style.image}>
-                    <img src={sofa} alt="" />
-                    <button>
-                        <img src={viewProduct} alt="" />
+                    {
+                        viewProduct ? <img src={productGit} alt="" /> : <img src={productImg} alt="" />
+                    }
+                    <button onClick={handleClick}>
+                    {
+                        viewProduct ? <img src={close} className={style.close} /> : <img src={view} alt="" />
+                    }
                     </button>
                 </div>
             </div>
